b9a283ca22a59fb8080f25ee67bec9803b589643
Laurena Fayad <laurenafayad@gmail.com>
2022-02-13 12:58:58 +0200

    Added the like_post.php code

--- like_post.php
+++ like_post.php
@@ -3,18 +3,15 @@
 include("db_info.php");
 
 // LIKE POST - ADD ENTRY TO POST_LIKE TABLE
-function LikePost ($user1_id, $user2_id) {
+function likePost ($user_id, $post_id) {
     global $mysqli;
-    $query = $mysqli->prepare(
-        "UPDATE relationship SET status='friend' 
-        WHERE (status='pending' AND 
-        ((user1_id =? AND user2_id =?) OR (user1_id =? AND user2_id =?)))");
+    $query = $mysqli->prepare("INSERT INTO post_like (post_id, account_id) VALUES (?,?)");
 
-    $query->bind_param("iiii", $user1_id, $user2_id, $user2_id, $user1_id);
+    $query->bind_param("ii", $post_id, $user_id);
     $query->execute();
-    $query->store_result();
 
     $query->close();
     $mysqli->close();
 }
 ?>
+
