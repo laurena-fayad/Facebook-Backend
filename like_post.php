c482e4e7b58d150d970003402edd51e43feec166
Laurena Fayad <laurenafayad@gmail.com>
2022-02-13 13:28:19 +0200

    Added validation to like_post

--- like_post.php
+++ like_post.php
@@ -3,15 +3,27 @@
 include("db_info.php");
 
 // LIKE POST - ADD ENTRY TO POST_LIKE TABLE
-function likePost ($user_id, $post_id) {
-    global $mysqli;
-    $query = $mysqli->prepare("INSERT INTO post_like (post_id, account_id) VALUES (?,?)");
+global $mysqli;
+$query = $mysqli->prepare("SELECT * FROM post_like WHERE (post_id = ? AND account_id = ?)");
+$query->bind_param("ii", $post_id, $user_id);
+$query->execute();
+$query->store_result();
+$num_rows = $query->num_rows;
 
-    $query->bind_param("ii", $post_id, $user_id);
-    $query->execute();
+$array_response = [];
 
-    $query->close();
-    $mysqli->close();
+if($num_rows != 0){
+    $array_response["error"] = "You already liked this post.";
+    $json_response = json_encode($array_response);
+    echo $json_response;
+
+    return false;
 }
-?>
 
+$query = $mysqli->prepare("INSERT INTO post_like (post_id, account_id) VALUES (?,?)");
+$query->bind_param("ii", $post_id, $user_id);
+$query->execute();
+
+$query->close();
+$mysqli->close();
+?>
