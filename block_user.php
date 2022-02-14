efdecf5b8fb2235d883cc729a3698333a3089334
Laurena Fayad <97550673+laurena-fayad@users.noreply.github.com>
2022-02-14 14:09:50 +0200

    Added get  values in block_user.php

--- block_user.php
+++ block_user.php
@@ -1,18 +1,27 @@
 
 <?php
 
+header("Access-Control-Allow-Origin: *");
+
 include("db_info.php");
 
+//GET $user id values
+$user1_id =  $_GET[$user1_id];
+$user2_id = $_GET[$user2_id];
+
 // BLOCK - ADD RELATIONSHIP ENTRY AS BLOCKED
-function block ($user1_id, $user2_id) {
-    global $mysqli;
-    $query = $mysqli->prepare("INSERT INTO relationship (user1_id, user2_id, status) VALUES (?,?,'blocked')");
+$array_response = [];
+global $mysqli;
+$query = $mysqli->prepare("INSERT INTO relationship (user1_id, user2_id, status) VALUES (?,?,'blocked')");
 
-    $query->bind_param("ii", $user1_id, $user2_id);
-    $query->execute();
+$query->bind_param("ii", $user1_id, $user2_id);
+$query->execute();
 
-    $query->close();
-    $mysqli->close();
-}
+$array_response["status"] = "Friend blocked successfully.";
+$json_response = json_encode($array_response);
+
+
+$query->close();
+$mysqli->close();
 
 ?>
