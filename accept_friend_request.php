74d8be7d99a36045867e6a61859a0d583f7539a5
Laurena Fayad <97550673+laurena-fayad@users.noreply.github.com>
2022-02-14 14:08:58 +0200

    Added get values to accept_friend _request.php

--- accept_friend_request.php
+++ accept_friend_request.php
@@ -1,22 +1,44 @@
-
 <?php
 
+header("Access-Control-Allow-Origin: *");
 include("db_info.php");
 
+//GET $user id values
+$user1_id =  $_GET[$user1_id];
+$user2_id = $_GET[$user2_id];
+
 // ACCEPT FRIEND REQUEST - UPGRADE STATUS TO FRIENDS
-function acceptRequest ($user1_id, $user2_id) {
-    global $mysqli;
+global $mysqli;
+
+// CHECK IF ALREADY FRIENDS
+$query = $mysqli->prepare("SELECT status FROM relationship WHERE ((user1_id = ? AND user2_id = ? AND status = 'friend') OR (user1_id = ? AND user2_id = ? AND status = 'friend'))");
+$query->bind_param("iiii", $user1_id, $user2_id, $user2_id, $user1_id);
+$query->execute();
+$query->store_result();
+$num_rows = $query->num_rows;
+
+$array_response = [];
+
+if($num_rows == 0){
     $query = $mysqli->prepare(
         "UPDATE relationship SET status='friend' 
         WHERE (status='pending' AND 
         ((user1_id =? AND user2_id =?) OR (user1_id =? AND user2_id =?)))");
-
+    
     $query->bind_param("iiii", $user1_id, $user2_id, $user2_id, $user1_id);
     $query->execute();
     $query->store_result();
+    $array_response["status"] = "Friend request accepted successfully.";
+    $json_response = json_encode($array_response);
 
-    $query->close();
-    $mysqli->close();
+}elseif($status == 'friend'){
+    $array_response["error"] = "Users are already friends";
+    $json_response = json_encode($array_response);
+
+    return false;
 }
+
+$query->close();
+$mysqli->close();
 
 ?>
