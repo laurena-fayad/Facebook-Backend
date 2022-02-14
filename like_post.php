56b5f8f7f83db64372aab1798b7f8203f3efd135
Laurena Fayad <97550673+laurena-fayad@users.noreply.github.com>
2022-02-14 14:06:26 +0200

    Added get values to like-post.php

--- like_post.php
+++ like_post.php
@@ -2,6 +2,10 @@
 
 include("db_info.php");
 
+//GET $user/post id values
+$user_id =  $_GET[$user_id];
+$post_id = $_GET[$post_id];
+
 // LIKE POST - ADD ENTRY TO POST_LIKE TABLE
 global $mysqli;
 $query = $mysqli->prepare("SELECT * FROM post_like WHERE (post_id = ? AND account_id = ?)");
@@ -15,7 +19,6 @@ $array_response = [];
 if($num_rows != 0){
     $array_response["error"] = "You already liked this post.";
     $json_response = json_encode($array_response);
-    echo $json_response;
 
     return false;
 }
@@ -23,6 +26,9 @@ if($num_rows != 0){
 $query = $mysqli->prepare("INSERT INTO post_like (post_id, account_id) VALUES (?,?)");
 $query->bind_param("ii", $post_id, $user_id);
 $query->execute();
+
+$array_response["status"] = "Post liked succesfully.";
+$json_response = json_encode($array_response);
 
 $query->close();
 $mysqli->close();
