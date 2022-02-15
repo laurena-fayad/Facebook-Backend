73cef29faf0647f8d67ac5471d34041979659817
MarlyKhoury <marlykh625@gmail.com>
2022-02-15 14:33:17 +0200

    edit and make a post features

--- post.php
+++ post.php
@@ -21,6 +21,24 @@ header("Access-Control-Allow-Headers: *");
         echo json_encode($array_response);
     };
      
-
-
+// MAKE A POST
+    function f_post($token) {
+        include ("db_info.php");
+        $post=$_POST["post"];
+        $tokenParts = explode('.', $token);
+        $payload = base64_decode($tokenParts[1]);
+        $data = json_decode($payload, true);
+        $user_id=$data["id"];
+        $date= date("Y-m-d H:i:s");
+        
+        $query = $mysqli->prepare("INSERT INTO user_post (account_id, post_text, post_date) VALUES(?,?,?)");
+        $query->bind_param("iss", $user_id, $post, $date);
+        $query->execute();
+        
+        $array_response = [];
+        $array_response["status"] = "You've just posted!";
+        $array_response["date"] = $date;
+        echo json_encode($array_response);
+    }
+  
 ?>
