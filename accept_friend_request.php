b039e5a03ba6b9695f601c1afb178f8a79d66d32
Laurena Fayad <laurenafayad@gmail.com>
2022-02-15 09:21:17 +0200

    Minor changes to the accept api

--- accept_friend_request.php
+++ accept_friend_request.php
@@ -4,36 +4,42 @@ header("Access-Control-Allow-Origin: *");
 include("db_info.php");
 
 //GET $user id values
-$user1_id =  $_GET[$user1_id];
-$user2_id = $_GET[$user2_id];
+if(isset($_GET['user1']) && isset($_GET['user2'])){
+    $user1_id =  $_GET["user1"];
+    $user2_id =  $_GET["user2"];    
+}
 
 // ACCEPT FRIEND REQUEST - UPGRADE STATUS TO FRIENDS
 global $mysqli;
 
 // CHECK IF ALREADY FRIENDS
-$query = $mysqli->prepare("SELECT status FROM relationship WHERE ((user1_id = ? AND user2_id = ? AND status = 'friend') OR (user1_id = ? AND user2_id = ? AND status = 'friend'))");
+$query = $mysqli->prepare(
+    "SELECT * 
+    FROM relationship 
+    WHERE (status = 'friend' AND ((user1_id = ? AND user2_id = ?) OR (user1_id = ? AND user2_id = ?)));");
 $query->bind_param("iiii", $user1_id, $user2_id, $user2_id, $user1_id);
 $query->execute();
 $query->store_result();
 $num_rows = $query->num_rows;
-
 $array_response = [];
 
 if($num_rows == 0){
     $query = $mysqli->prepare(
         "UPDATE relationship SET status='friend' 
         WHERE (status='pending' AND 
-        ((user1_id =? AND user2_id =?) OR (user1_id =? AND user2_id =?)))");
+        ((user1_id =? AND user2_id =?) OR (user1_id =? AND user2_id =?)));");
     
     $query->bind_param("iiii", $user1_id, $user2_id, $user2_id, $user1_id);
     $query->execute();
     $query->store_result();
     $array_response["status"] = "Friend request accepted successfully.";
     $json_response = json_encode($array_response);
+    echo $json_response;
 
-}elseif($status == 'friend'){
+}else{
     $array_response["error"] = "Users are already friends";
     $json_response = json_encode($array_response);
+    echo $json_response;
 
     return false;
 }
